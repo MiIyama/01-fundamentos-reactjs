0612500e568564a1ffa91b5b9cd200ef8796850c
MiIyama <naomi898@gmail.com>
2022-09-18 01:03:53 +0000

    Aula Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Post } from "./Post";
 export function App() {
   return (
     <>
-      <Post />
-      <Post />
+      <Post author="Ana" content="Lorem ipsum" />
+      <Post author="Bela" content="Lorem ipsum Lorem ipsum" />
       <Post />
       <Post />
       <Post />
