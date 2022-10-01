6c5f7d143977706fb4d1fe1420793b87f68252a3
MiIyama <naomi898@gmail.com>
2022-10-01 16:10:49 -0300

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 import "./global.css";
+
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+
 export function App() {
   return (
     <>
       <Header />
-      <Post author="Ana" content="Lorem ipsum" />
-      <Post author="Bela" content="Lorem ipsum Lorem ipsum" />
-      <Post />
-      <Post />
-      <Post />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Ana" content="Lorem ipsum" />
+          <Post author="Bela" content="Lorem ipsum Lorem ipsum" />
+        </main>
+      </div>
     </>
   );
 }
