b1c67938b6d9b272ecc5e0cbc4f2326f27923812
MiIyama <naomi898@gmail.com>
2022-10-02 01:53:28 +0000

    Aula - Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,27 @@
 import styles from "./Sidebar.module.css";
-
+import { PencilLine } from "phosphor-react";
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img className={styles.cover} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ86kDhi2KH9HPN3Dk1fh0Kwbh2POfiYtMZw&usqp=CAU"></img>
+      <img
+        className={styles.cover}
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ86kDhi2KH9HPN3Dk1fh0Kwbh2POfiYtMZw&usqp=CAU"
+      ></img>
 
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/44329442?v=4"
+        />
         <strong>Naomi Iyama</strong>
         <span>Web developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
