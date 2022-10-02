da2c54e813bd7d3873d9ee534af020c32aaa48e2
MiIyama <naomi898@gmail.com>
2022-10-02 03:39:01 +0000

    Aula - Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,37 @@
+import styles from "./Post.module.css";
+
+export function Post(props) {
+  return (
+    <>
+      <article className={styles.post}>
+        <header>
+          <div className={styles.author}>
+            <img
+              className={styles.avatar}
+              src="https://avatars.githubusercontent.com/u/44329442?v=4"
+            />
+            <div className={styles.authorInfo}>
+              <strong>Ingrid Naomi</strong>
+              <span>Web Developer</span>
+            </div>
+          </div>
+          <time title="11 de maio as 08:13h" dateTime="2022-05-11 08:13:30">
+            Publicado há 1h
+          </time>
+        </header>
+
+        <div className={styles.content}>
+          <p>Fala galera</p>
+          <p>Blanditiis odit ipsum similique illo noex quo beatae nulla?</p>
+          <p>Officiis, totam debitis quae aperiam dote dele</p>
+          <p>
+            <a href="">jane.design/doctorcare</a>
+          </p>
+          <p>
+            <a href="">#novoprojeto #nlw</a>
+          </p>
+        </div>
+      </article>
+    </>
+  );
+}
